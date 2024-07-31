f8c07a7a3cf4e6355308cc0d2e747e9994212624
chirayu patel <Chirayupatel911@gmail.com>
2024-07-31 14:44:32 -0400

    added generic functions

--- template_functions.py
+++ template_functions.py
@@ -1,1 +1,27 @@
 # Write functions are can be later with other functions
+from decimal import Decimal
+from datetime import datetime
+
+ALLOWED_EXTENSIONS = {'txt', 'csv', 'CSV'}
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def convert_to_yyyy_mm_dd(date_str):
+    try:
+        date_obj = datetime.strptime(date_str, '%Y/%m/%d')
+        return date_obj.strftime('%Y-%m-%d')
+    except ValueError:
+        try:
+            date_obj = datetime.strptime(date_str, '%d/%m/%Y')
+            return date_obj.strftime('%Y-%m-%d')
+        except ValueError:
+            try:
+                date_obj = datetime.strptime(date_str, '%m/%d/%Y')
+                return date_obj.strftime('%Y-%m-%d')
+            except ValueError:
+                print("Unrecognized date format", date_str)
+                return None
